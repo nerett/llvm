b98b43aabc8728d64609d2f279fc13e1169cc279
nerett <pt-mvb@inbox.lv>
2024-11-18 23:51:16 +0300

    updated app.s according to new ISA

--- gui_app/app.s
+++ gui_app/app.s
@@ -1,35 +1,38 @@
-entry
-    ALLOCM r0
-    ALLOCM r1
+mem
+    ALLOCM r10
+    ALLOCM r11
 
-    LIFETIME_START r0
-    INM r0
+    BR entry
+
+entry
+    LIFETIME_START r10
+    INM r10
 
     MOV r20 200
     MOV r21 201
     MOV r22 202
 
-    STR r0 r20 r20 0
-    STR r0 r21 r20 1
-    STR r0 r22 r20 0
+    STR r10 r20 r20 0
+    STR r10 r21 r20 1
+    STR r10 r22 r20 0
 
-    STR r0 r20 r21 0
-    STR r0 r21 r21 1
-    STR r0 r22 r21 1
+    STR r10 r20 r21 0
+    STR r10 r21 r21 1
+    STR r10 r22 r21 1
 
-    STR r0 r20 r22 1
-    STR r0 r21 r22 1
-    STR r0 r22 r22 0
+    STR r10 r20 r22 1
+    STR r10 r21 r22 1
+    STR r10 r22 r22 0
 
-    LIFETIME_START r1
-    INM r1
+    LIFETIME_START r11
+    INM r11
 
     MOV r3 0
     BR loop_epoch
 
 loop_epoch
     MOV r4 0
-    BLE r3 2 loop_x exit
+    BLE r3 1000 loop_x exit
 
 loop_x
     MOV r5 0
@@ -43,46 +46,46 @@ body
 
     SUB r7 r4 1
     SUB r8 r5 1
-    LDA r6 r0 r7 r8
+    LDA r6 r10 r7 r8
 
     SUB r7 r4 1
     SUB r8 r5 0
-    LDA r6 r0 r7 r8
+    LDA r6 r10 r7 r8
 
     SUB r7 r4 1
     ADD r8 r5 1
-    LDA r6 r0 r7 r8
+    LDA r6 r10 r7 r8
 
     SUB r7 r4 0
     ADD r8 r5 1
-    LDA r6 r0 r7 r8
+    LDA r6 r10 r7 r8
 
     ADD r7 r4 1
     ADD r8 r5 1
-    LDA r6 r0 r7 r8
+    LDA r6 r10 r7 r8
 
     ADD r7 r4 1
     SUB r8 r5 0
-    LDA r6 r0 r7 r8
+    LDA r6 r10 r7 r8
 
     ADD r7 r4 1
     SUB r8 r5 1
-    LDA r6 r0 r7 r8
+    LDA r6 r10 r7 r8
 
     SUB r7 r4 1
     SUB r8 r5 1
-    LDA r6 r0 r7 r8
+    LDA r6 r10 r7 r8
 
     SUB r7 r4 0
     SUB r8 r5 1
-    LDA r6 r0 r7 r8
+    LDA r6 r10 r7 r8
 
-    LDA r6 r0 r4 r5
+    LDA r6 r10 r4 r5
 
     BEQ r6 3 draw_alive draw_dead
 
 draw_alive
-    STR r1 r4 r5 1
+    STR r11 r4 r5 1
     PUT_PIXEL r4 r5 16711935
 
     BR body_end
@@ -102,12 +105,12 @@ body_end
     BR swap
 
 swap
-    CPYM r0 r1
-    INM r1
+    CPYM r10 r11
+    INM r11
 
     BR loop_epoch
 
 exit
-    FREEM r0
-    FREEM r1
+    FREEM r10
+    FREEM r11
     HLT
