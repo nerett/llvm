53563fb7bdb98f1b7b17b53a1c57087b35b9fbe8
nerett <pt-mvb@inbox.lv>
2024-11-24 17:22:55 +0300

    FINALLY FIXED ASSEMBLY and it just works

--- gui_app/app.s
+++ gui_app/app.s
@@ -6,7 +6,6 @@ mem
 
 entry
     LIFETIME_START r10
-    INM r10
 
     MOV r20 200
     MOV r21 201
@@ -25,21 +24,20 @@ entry
     STR r10 r22 r22 0
 
     LIFETIME_START r11
-    INM r11
 
     MOV r3 0
-    BR loop_epoch
+    BR loop_epoch_begin
 
-loop_epoch
+loop_epoch_begin
     MOV r4 0
-    BLE r3 1000 loop_x exit
+    BLE r3 1000 loop_x_begin exit
 
-loop_x
+loop_x_begin
     MOV r5 0
-    BLE r4 512 loop_y loop_epoch
+    BLE r4 512 loop_y_begin loop_epoch_end
 
-loop_y
-    BLE r5 512 body loop_x
+loop_y_begin
+    BLE r5 512 body loop_x_end
 
 body
     MOV r6 0
@@ -72,43 +70,48 @@ body
     SUB r8 r5 1
     LDA r6 r10 r7 r8
 
-    SUB r7 r4 1
-    SUB r8 r5 1
-    LDA r6 r10 r7 r8
-
     SUB r7 r4 0
     SUB r8 r5 1
     LDA r6 r10 r7 r8
 
-    LDA r6 r10 r4 r5
+    BR if_1
 
+if_1
+    BEQ r6 3 draw_alive if_2
+
+if_2
+    LDA r6 r10 r4 r5
     BEQ r6 3 draw_alive draw_dead
 
 draw_alive
     STR r11 r4 r5 1
     PUT_PIXEL r4 r5 16711935
 
-    BR body_end
+    BR loop_y_end
 
 draw_dead
     PUT_PIXEL r4 r5 255
 
-    BR body_end
+    BR loop_y_end
 
-body_end
+loop_y_end
+    ADD r5 r5 1
+    BR loop_y_begin
+
+loop_x_end
+    ADD r4 r4 1
+    BR loop_x_begin
+
+loop_epoch_end
     FLUSH
 
-    ADD r3 r3 1
-    ADD r4 r4 1
-    ADD r5 r5 1
-
-    BR swap
-
-swap
-    CPYM r10 r11
+    SWAP r10 r11
     INM r11
 
-    BR loop_epoch
+    ADD r3 r3 1
+
+    MOV r31 0
+    BR loop_epoch_begin
 
 exit
     FREEM r10
